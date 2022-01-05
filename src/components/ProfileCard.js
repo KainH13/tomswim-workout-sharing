6d84983ce4cd3add46adf42112fc039c6eba3e66
Kai Neuhold-Huber <kneuholdhuber@gmail.com>
2022-01-04 16:19:57 -0800

    ProfileCard component started and ready for deep dive

---
+++ src/components/ProfileCard.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ProfileCard = (props) => {
+  const { testUser } = props;
+
+  return (
+    <div>
+      <h1 className="text-center">Profile Card</h1>
+    </div>
+  )
+}
+
+export default ProfileCard;
--- src/views/Profile.js
+++ src/views/Profile.js
@@ -1,11 +1,32 @@
 import React from "react";
 import Nav from "../components/Nav";
+import CreateWorkout from "../components/CreateWorkout";
+import WorkoutFeed from "../components/WorkoutFeed";
+import ProfileCard from "../components/ProfileCard";
 
 const Profile = (props) => {
+  const { workouts, setWorkouts } = props
+
+  const testUser = {
+    username: "TomSwim",
+    about: "Lifelong swimmer who love coaching and sharing workouts!",
+    workouts: "500"
+  }
+
   return (
     <div className="container">
       <Nav page="profile" />
-      <h1>Profile View</h1>
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <ProfileCard testUser={testUser} />
+            <CreateWorkout workouts={workouts} setWorkouts={setWorkouts} />
+          </div>
+          <div className="col">
+            <WorkoutFeed workouts={workouts} />
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
