e4ad30e96975440c243bfbfd3985821599bcc8c3
Kai Neuhold-Huber <kneuholdhuber@gmail.com>
2022-01-07 12:50:35 -0800

    moved directory up to encompass front and back end + began work on back end

---
+++ back_end/flask_app/controllers/users.py
@@ -0,0 +1,41 @@
+import bcrypt
+from flask_app.models.user import User
+from flask_app import app
+from flask import jsonify, request
+from flask_bcrypt import Bcrypt
+bcrypt = Bcrypt(app)
+
+# Create
+@app.route('/api/user/register', methods=['POST'])
+def create_user():
+    pw_hash = bcrypt.generate_password_hash(request.args["password"])
+    data = {
+        "username": request.args["username"],
+        "email": request.args["email"],
+        "password": pw_hash,
+    }
+    
+    return jsonify(User.create(data))
+
+# Read
+@app.route('/api/users')
+def all_users():
+    return jsonify(User.get_all_users())
+
+@app.route('/api/users/emails')
+def all_emails():
+    return jsonify(User.get_all_emails())
+
+# @app.route('/users')
+# def users():
+#     return jsonify(User.get_all_json())
+
+# @app.route('/create/user', methods=['POST'])
+# def create_user():
+#     print(request.form)
+#     data = {
+#         'user_name' : request.form['user_name'],
+#         'email' : request.form['email']
+#     }
+#     return jsonify(User.save(data))
+    
---
+++ back_end/flask_app/models/user.py
@@ -0,0 +1,34 @@
+from flask_app.config.mysqlconnection import connectToMySQL
+
+class User:
+    db = "tomswim_db"
+    def __init__(self, data):
+        self.id = data['id']
+        self.username = data['username']
+        self.email = data['email']
+        self.password = data['password']
+        self.about = data['about']
+        self.profile_img = data['profile_img']
+        self.created_at = data['created_at']
+        self.updated_at = data['updated_at']
+
+    # Create
+    @classmethod
+    def create(cls, data):
+        query = "INSERT INTO users (username, email, password) VALUES(%(username)s, %(email)s, %(password)s);"
+        return connectToMySQL(cls.db).query_db(query, data)
+
+    # Read
+    @classmethod
+    def get_all_users(cls):
+        query = "SELECT * FROM users;"
+        return connectToMySQL(cls.db).query_db(query)
+
+    @classmethod
+    def get_all_emails(cls):
+        query = "SELECT * FROM users;"
+        results = connectToMySQL(cls.db).query_db(query)
+        all_emails = []
+        for entry in results:
+            all_emails.append(entry['email'])
+        return all_emails
